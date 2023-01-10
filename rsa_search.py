d145cc98868b8e343457370e7ac56a14d72e37c4
KotenAngered <113610915+KotenAngered@users.noreply.github.com>
2023-01-10 16:53:31 +0100

    Add files via upload
    It was made for personal use and won't be ever updated unless critical bugs will be found.

---
+++ rsa_search.py
@@ -0,0 +1,36 @@
+import re
+import sys
+from colorama import Fore
+
+pattern = r"[a-zA-Z0-9+/=\n]+"
+
+def rsa_key_search(path):
+    with open("{path}", "w") as f:
+        result = re.findall(pattern, path)
+        if result:
+            with open("rsa_keys.txt", "w") as s:
+                s.write(result)
+                s.close()
+                f.close()
+                input("Data saved succesfully!\nPress anything to close the script.")
+                sys.exit()
+        else:
+                input("Something went wrong.")
+                         
+
+def main():
+    while True:
+               print("Welcome to RSA Key searcher!")
+               print("Please, choose what do you want to do. ")
+               print("1. Search for RSA Keys.")
+               
+               
+               user_choice = int(input("Choose variant: "))                                                                                   
+               choice_list = [1]
+               if user_choice in choice_list:
+                    print(Fore.RED + "Please, make sure you are running this script in the same folder as files with RSA keys.")
+                    file_name = input("Please tell me the file name. Example: rsa_key.txt\nTell it: ")
+                    rsa_key_search(file_name)
+                    
+if __name__ == "__main__" :
+    main()                  
